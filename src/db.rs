a115d42c65978e1529327c8947a789c17955ffc2
Gabriele Falace <gabrielefalace@gmail.com>
2025-08-15 14:31:20 +0200

    Update db.rs

--- src/db.rs
+++ src/db.rs
@@ -1,1 +1,25 @@
+use mongodb::options::ClientOptions;
+use mongodb::{Client, Database};
+use tokio::sync::OnceCell;
 
+static DB: OnceCell<Database> = OnceCell::const_new();
+
+
+pub async fn get_db() -> mongodb::error::Result<Database> {
+    let database_url = "mongodb://localhost:27017/?directConnection=true";
+    println!(">>> Connecting to DB: {:?}", database_url);
+    let client_options = ClientOptions::parse(database_url).await?;
+    let client = Client::with_options(client_options)?;
+    let db = client.database("my-db-name");
+    Ok(db)
+}
+
+
+pub async fn get_db_once() -> &'static Database {
+    DB.get_or_init(|| async {
+        let url = "mongodb://localhost:27017/?directConnection=true";
+        println!(">>> Connecting to DB: {url}");
+        let client = Client::with_uri_str(url).await.expect("Mongo connection failed");
+        client.database("my-db-name")
+    }).await
+}
